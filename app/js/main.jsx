e1ab60862982d47fb4558329a5e41325920ae32d
yzuniversal <yzukonta@gmail.com>
2016-07-23 00:24:29 +0200

    Added material-ui package

--- app/js/main.jsx
+++ app/js/main.jsx
@@ -1,7 +1,11 @@
 const React = require('react');
 const ReactDom = require('react-dom');
+const injectTapEventPlugin = require('react-tap-event-plugin');
 const App = require('@components/App');
 
 document.addEventListener('DOMContentLoaded', () => {
+  //  Required by Material-UI:
+  injectTapEventPlugin();
+
   ReactDom.render(<App />, document.getElementById('react'));
 });
