cf98293cd8384ee29017fceea9edbba32f46b560
yzuniversal <yzukonta@gmail.com>
2016-07-31 18:25:03 +0200

    Added a way to clear input text in SearchBar.jsx

--- app/js/components/SearchBar.jsx
+++ app/js/components/SearchBar.jsx
@@ -1,27 +1,60 @@
 const React = require('react');
 const {
   AutoComplete,
+  IconButton,
   Paper,
 } = require('material-ui');
 const {
   ActionSearch,
+  NavigationClose,
 } = require('material-ui/svg-icons');
 const actionCreators = require('@js/actionCreators');
 const consts = require('@js/constants');
 
 
-function SearchBar(props) {
-  const source = ['foo', 'bar'];
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return <Paper style={styles.wrapper} >
-    <ActionSearch style={styles.searchIcon} />
-    <AutoComplete style={styles.searchField}
-      inputStyle={styles.searchInput}
-      dataSource={source}
-      openOnFocus
-      fullWidth
-      underlineShow={false} />
-  </Paper>;
+    this.state = {
+      input: '',
+    };
+  }
+
+  inputFunc(text) {
+    this.setState({
+      input: text,
+    });
+  }
+
+  clearInputFunc() {
+    this.setState({
+      input: '',
+    });
+  }
+
+  render() {
+    const source = ['foo', 'bar'];
+
+    return <Paper style={styles.wrapper} >
+      <ActionSearch style={styles.searchIcon} />
+      <AutoComplete style={styles.searchField}
+        inputStyle={styles.searchInput}
+        dataSource={source}
+        searchText={this.state.input}
+        openOnFocus
+        fullWidth
+        underlineShow={false}
+        onUpdateInput={this.inputFunc.bind(this)} />
+      {
+        this.state.input !== '' &&
+        <IconButton style={styles.closeIcon}
+          onClick={this.clearInputFunc.bind(this)} >
+          <NavigationClose />
+        </IconButton>
+      }
+    </Paper>;
+  }
 }
 
 const styles = {
@@ -37,14 +70,19 @@ const styles = {
   },
   searchInput: {
     fontSize: 18,
-    width: 'calc(100% - 8px - 48px)',
+    width: 'calc(100% - 48px - 48px)',
     paddingLeft: 48,
-    paddingRight: 8,
+    paddingRight: 48,
   },
   searchIcon: {
     position: 'absolute',
     top: 12 + 4,
     left: 12,
+  },
+  closeIcon: {
+    position: 'absolute',
+    top: 4,
+    right: 4,
   },
 };
 
