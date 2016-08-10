f8b71691ae57834614a1d2ef4830edac71be2a63
yzuniversal <yzukonta@gmail.com>
2016-08-10 22:38:48 +0200

    Refactored WeatherStore.init

--- app/js/stores/WeatherStore/WeatherStore.init.js
+++ app/js/stores/WeatherStore/WeatherStore.init.js
@@ -9,6 +9,31 @@ const city = 'Warsaw, PL';
 
 function init() {
   //  We only return this promise for the purposes of testing:
+  return Promise.all([getDataFromStorage()])
+  .then(([state]) => {
+    // Minimum period of time to wait before we load another forecast (in ms):
+    const timeToWait = 1000 * 60 * 60 * 4;
+    const lastChecked = state.get('lastChecked');
+
+    if (lastChecked && Date.now() - lastChecked < timeToWait) {
+      actionCreators.initWeatherStore(state);
+
+      return undefined;
+    }
+    else {
+      //  Return for testing:
+      return fetchNewData(state)
+      .then(state => {
+        actionCreators.initWeatherStore(state);
+        storageHelpers.setInStorage({
+          weatherData: JSON.stringify(state.toJS()),
+        });
+      });
+    }
+  });
+}
+
+function getDataFromStorage() {
   return storageHelpers.getFromStorage('weatherData')
   .then(weatherData => {
     if (weatherData) {
@@ -24,96 +49,85 @@ function init() {
     else {
       return new Immutable.Map();
     }
-  })
-  .then(state => {
-    // Minimum period of time to wait before we load another forecast (in ms):
-    const timeToWait = 1000 * 60 * 60 * 4;
-    const lastChecked = state.get('lastChecked');
-    const APPID = '8abe6b7721a64a5e06660f56047d1f59';
-    const BASEURL = 'http://api.openweathermap.org/data/2.5/';
+  });
+}
 
-    if (lastChecked && Date.now() - lastChecked < timeToWait) {
-      actionCreators.initWeatherStore(state);
+function fetchNewData(state) {
+  const APPID = '8abe6b7721a64a5e06660f56047d1f59';
+  const BASEURL = 'http://api.openweathermap.org/data/2.5/';
 
-      return undefined;
+  const today = fetch(`${BASEURL}weather?q=${city}&APPID=${APPID}`)
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    else {
+      throw new Error('Failed to receive weather data for today');
+    }
+  });
+
+  const forecast = fetch(`${BASEURL}forecast?q=${city}&APPID=${APPID}`)
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    else {
+      throw new Error('Failed to receive weather forecast');
+    }
+  });
+
+  return Promise.all([today, forecast])
+  .then(formatData)
+  .then(data => {
+    return state
+    .mergeDeep(data)
+    .set('lastChecked', Date.now());
+  });
+}
+
+function formatData([today, forecast]) {
+  const weather = {
+    location: today.name,
+    today: {
+      temp: today.main.temp,
+      status: today.weather[0].main,
+      icon: today.weather[0].icon,
+    },
+    forecast: [],
+  };
+
+  const now = moment().startOf('day');
+  let currentDay = 0;
+
+  forecast.list
+  .map(entry => ({
+    date: moment.utc(entry.dt_txt),
+    weather: {
+      temp: entry.main.temp,
+      status: entry.weather[0].main,
+      icon: entry.weather[0].icon,
+    },
+  }))
+  .filter(entry => isDay(entry.date.hour()) || isNight(entry.date.hour()))
+  .forEach(entry => {
+    if (isDay(entry.date.hour()) && entry.date.date() !== now.date()) {
+      currentDay++;
     }
 
-    const today = fetch(`${BASEURL}weather?q=${city}&APPID=${APPID}`)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      else {
-        throw new Error('Failed to receive weather data for today');
-      }
-    });
-
-    const forecast = fetch(`${BASEURL}forecast?q=${city}&APPID=${APPID}`)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      else {
-        throw new Error('Failed to receive weather forecast');
-      }
-    });
-
-    return Promise.all([today, forecast])
-    .then(([today, forecast]) => {
-      const weather = {
-        location: today.name,
-        today: {
-          temp: today.main.temp,
-          status: today.weather[0].main,
-          icon: today.weather[0].icon,
-        },
-        forecast: [],
+    if (!weather.forecast[currentDay]) {
+      weather.forecast[currentDay] = {
+        date: now.clone().add(currentDay, 'd'),
       };
+    }
 
-      const now = moment().startOf('day');
-      let currentDay = 0;
+    const time = isDay(entry.date.hour()) ? 'day' : 'night';
 
-      forecast.list
-      .map(entry => ({
-        date: moment.utc(entry.dt_txt),
-        weather: {
-          temp: entry.main.temp,
-          status: entry.weather[0].main,
-          icon: entry.weather[0].icon,
-        },
-      }))
-      .filter(entry => isDay(entry.date.hour()) || isNight(entry.date.hour()))
-      .forEach(entry => {
-        if (isDay(entry.date.hour()) && entry.date.date() !== now.date()) {
-          currentDay++;
-        }
-
-        if (!weather.forecast[currentDay]) {
-          weather.forecast[currentDay] = {
-            date: now.clone().add(currentDay, 'd'),
-          };
-        }
-
-        const time = isDay(entry.date.hour()) ? 'day' : 'night';
-
-        weather.forecast[currentDay][time] = entry.weather;
-      });
-
-      weather.forecast = weather.forecast.slice(0, 5);
-
-      return weather;
-    })
-    .then(weather => {
-      state = state
-      .mergeDeep(weather)
-      .set('lastChecked', Date.now());
-
-      actionCreators.initWeatherStore(state);
-      storageHelpers.setInStorage({
-        weatherData: JSON.stringify(state.toJS()),
-      });
-    });
+    weather.forecast[currentDay][time] = entry.weather;
   });
+
+  weather.forecast = weather.forecast.slice(0, 5);
+
+  return weather;
 }
 
 //  The forecast is a list of objects containing weather data,
